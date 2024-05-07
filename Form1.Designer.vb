8ff3fff2728122d74d9a7c5ffbf42579d6f8c5dd
Guilherme Figueira <guibf2004@gmail.com>
2024-05-07 16:40:36 -0300

    login

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,16 +22,145 @@ Partial Class Form1
     'Não o modifique usando o editor de códigos.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.GunaLabel1 = New Guna.UI.WinForms.GunaLabel()
+        Me.GunaLabel2 = New Guna.UI.WinForms.GunaLabel()
+        Me.GunaLabel3 = New Guna.UI.WinForms.GunaLabel()
+        Me.GunaTextBox1 = New Guna.UI.WinForms.GunaTextBox()
+        Me.GunaTextBox2 = New Guna.UI.WinForms.GunaTextBox()
+        Me.GunaButton1 = New Guna.UI.WinForms.GunaButton()
+        Me.Guna2BorderlessForm1 = New Guna.UI2.WinForms.Guna2BorderlessForm(Me.components)
         Me.SuspendLayout()
+        '
+        'GunaLabel1
+        '
+        Me.GunaLabel1.AutoSize = True
+        Me.GunaLabel1.Font = New System.Drawing.Font("Julius Sans One", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GunaLabel1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(46, Byte), Integer), CType(CType(31, Byte), Integer), CType(CType(39, Byte), Integer))
+        Me.GunaLabel1.Location = New System.Drawing.Point(70, 26)
+        Me.GunaLabel1.Name = "GunaLabel1"
+        Me.GunaLabel1.Size = New System.Drawing.Size(396, 70)
+        Me.GunaLabel1.TabIndex = 0
+        Me.GunaLabel1.Text = "TÁNAMESA!"
+        '
+        'GunaLabel2
+        '
+        Me.GunaLabel2.AutoSize = True
+        Me.GunaLabel2.Font = New System.Drawing.Font("Libre Caslon Display", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GunaLabel2.Location = New System.Drawing.Point(32, 234)
+        Me.GunaLabel2.Name = "GunaLabel2"
+        Me.GunaLabel2.Size = New System.Drawing.Size(182, 58)
+        Me.GunaLabel2.TabIndex = 1
+        Me.GunaLabel2.Text = "SENHA"
+        '
+        'GunaLabel3
+        '
+        Me.GunaLabel3.AutoSize = True
+        Me.GunaLabel3.Font = New System.Drawing.Font("Libre Caslon Display", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GunaLabel3.Location = New System.Drawing.Point(32, 138)
+        Me.GunaLabel3.Name = "GunaLabel3"
+        Me.GunaLabel3.Size = New System.Drawing.Size(111, 58)
+        Me.GunaLabel3.TabIndex = 2
+        Me.GunaLabel3.Text = "CPF"
+        '
+        'GunaTextBox1
+        '
+        Me.GunaTextBox1.BackColor = System.Drawing.Color.Transparent
+        Me.GunaTextBox1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(251, Byte), Integer), CType(CType(228, Byte), Integer), CType(CType(196, Byte), Integer))
+        Me.GunaTextBox1.BorderColor = System.Drawing.Color.FromArgb(CType(CType(133, Byte), Integer), CType(CType(77, Byte), Integer), CType(CType(39, Byte), Integer))
+        Me.GunaTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.GunaTextBox1.FocusedBaseColor = System.Drawing.Color.FromArgb(CType(CType(251, Byte), Integer), CType(CType(228, Byte), Integer), CType(CType(196, Byte), Integer))
+        Me.GunaTextBox1.FocusedBorderColor = System.Drawing.Color.FromArgb(CType(CType(133, Byte), Integer), CType(CType(77, Byte), Integer), CType(CType(39, Byte), Integer))
+        Me.GunaTextBox1.FocusedForeColor = System.Drawing.SystemColors.ControlText
+        Me.GunaTextBox1.Font = New System.Drawing.Font("Libre Caslon Display", 30.0!)
+        Me.GunaTextBox1.Location = New System.Drawing.Point(217, 138)
+        Me.GunaTextBox1.Name = "GunaTextBox1"
+        Me.GunaTextBox1.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.GunaTextBox1.Radius = 12
+        Me.GunaTextBox1.SelectedText = ""
+        Me.GunaTextBox1.Size = New System.Drawing.Size(700, 60)
+        Me.GunaTextBox1.TabIndex = 3
+        '
+        'GunaTextBox2
+        '
+        Me.GunaTextBox2.BackColor = System.Drawing.Color.Transparent
+        Me.GunaTextBox2.BaseColor = System.Drawing.Color.FromArgb(CType(CType(251, Byte), Integer), CType(CType(228, Byte), Integer), CType(CType(196, Byte), Integer))
+        Me.GunaTextBox2.BorderColor = System.Drawing.Color.FromArgb(CType(CType(133, Byte), Integer), CType(CType(77, Byte), Integer), CType(CType(39, Byte), Integer))
+        Me.GunaTextBox2.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.GunaTextBox2.FocusedBaseColor = System.Drawing.Color.FromArgb(CType(CType(251, Byte), Integer), CType(CType(228, Byte), Integer), CType(CType(196, Byte), Integer))
+        Me.GunaTextBox2.FocusedBorderColor = System.Drawing.Color.FromArgb(CType(CType(133, Byte), Integer), CType(CType(77, Byte), Integer), CType(CType(39, Byte), Integer))
+        Me.GunaTextBox2.FocusedForeColor = System.Drawing.SystemColors.ControlText
+        Me.GunaTextBox2.Font = New System.Drawing.Font("Libre Caslon Display", 30.0!)
+        Me.GunaTextBox2.Location = New System.Drawing.Point(383, 234)
+        Me.GunaTextBox2.Name = "GunaTextBox2"
+        Me.GunaTextBox2.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.GunaTextBox2.Radius = 12
+        Me.GunaTextBox2.SelectedText = ""
+        Me.GunaTextBox2.Size = New System.Drawing.Size(534, 60)
+        Me.GunaTextBox2.TabIndex = 4
+        '
+        'GunaButton1
+        '
+        Me.GunaButton1.AnimationHoverSpeed = 0.07!
+        Me.GunaButton1.AnimationSpeed = 0.03!
+        Me.GunaButton1.BackColor = System.Drawing.Color.Transparent
+        Me.GunaButton1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(133, Byte), Integer), CType(CType(77, Byte), Integer), CType(CType(39, Byte), Integer))
+        Me.GunaButton1.BorderColor = System.Drawing.Color.Black
+        Me.GunaButton1.DialogResult = System.Windows.Forms.DialogResult.None
+        Me.GunaButton1.FocusedColor = System.Drawing.Color.Empty
+        Me.GunaButton1.Font = New System.Drawing.Font("Libre Caslon Display", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GunaButton1.ForeColor = System.Drawing.Color.White
+        Me.GunaButton1.Image = Nothing
+        Me.GunaButton1.ImageSize = New System.Drawing.Size(20, 20)
+        Me.GunaButton1.Location = New System.Drawing.Point(553, 330)
+        Me.GunaButton1.Name = "GunaButton1"
+        Me.GunaButton1.OnHoverBaseColor = System.Drawing.Color.FromArgb(CType(CType(151, Byte), Integer), CType(CType(143, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.GunaButton1.OnHoverBorderColor = System.Drawing.Color.Black
+        Me.GunaButton1.OnHoverForeColor = System.Drawing.Color.White
+        Me.GunaButton1.OnHoverImage = Nothing
+        Me.GunaButton1.OnPressedColor = System.Drawing.Color.Black
+        Me.GunaButton1.Radius = 8
+        Me.GunaButton1.Size = New System.Drawing.Size(364, 88)
+        Me.GunaButton1.TabIndex = 5
+        Me.GunaButton1.TabStop = False
+        Me.GunaButton1.Text = "ENTRAR"
+        Me.GunaButton1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Guna2BorderlessForm1
+        '
+        Me.Guna2BorderlessForm1.BorderRadius = 12
+        Me.Guna2BorderlessForm1.ContainerControl = Me
+        Me.Guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6R
+        Me.Guna2BorderlessForm1.DragStartTransparencyValue = 1.0R
+        Me.Guna2BorderlessForm1.ResizeForm = False
+        Me.Guna2BorderlessForm1.TransparentWhileDrag = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(244, Byte), Integer), CType(CType(187, Byte), Integer), CType(CType(108, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(963, 454)
+        Me.Controls.Add(Me.GunaButton1)
+        Me.Controls.Add(Me.GunaTextBox2)
+        Me.Controls.Add(Me.GunaTextBox1)
+        Me.Controls.Add(Me.GunaLabel3)
+        Me.Controls.Add(Me.GunaLabel2)
+        Me.Controls.Add(Me.GunaLabel1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents GunaLabel1 As Guna.UI.WinForms.GunaLabel
+    Friend WithEvents GunaLabel2 As Guna.UI.WinForms.GunaLabel
+    Friend WithEvents GunaLabel3 As Guna.UI.WinForms.GunaLabel
+    Friend WithEvents GunaTextBox1 As Guna.UI.WinForms.GunaTextBox
+    Friend WithEvents GunaTextBox2 As Guna.UI.WinForms.GunaTextBox
+    Friend WithEvents GunaButton1 As Guna.UI.WinForms.GunaButton
+    Friend WithEvents Guna2BorderlessForm1 As Guna.UI2.WinForms.Guna2BorderlessForm
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,29 @@
 ﻿Public Class Form1
+    Private Sub GunaTextBox1_TextChanged(sender As Object, e As EventArgs)
 
+    End Sub
+
+    Private Sub GunaTextBox2_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub GunaTextBox3_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub GunaLabel1_Click(sender As Object, e As EventArgs) Handles GunaLabel1.Click
+
+    End Sub
+
+    Private Sub GunaLabel2_Click(sender As Object, e As EventArgs) Handles GunaLabel2.Click
+
+    End Sub
+
+    Private Sub GunaTextBox1_TextChanged_1(sender As Object, e As EventArgs) Handles GunaTextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
