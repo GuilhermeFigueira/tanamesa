d3af15f721fd25f1563f31af91ef95119f057e46
Guilherme Figueira <guibf2004@gmail.com>
2024-05-08 21:39:24 -0300

    home

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.tanamesa.form_base
+            Me.MainForm = Global.tanamesa.home
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
---
+++ home.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Runtime.CompilerServices
+
+Public Class home
+    Private Sub btn_fechar_Click(sender As Object, e As EventArgs) Handles btn_fechar.Click
+        Close()
+    End Sub
+
+    Private Sub btn_minimizar_Click(sender As Object, e As EventArgs) Handles btn_minimizar.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub Guna2ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmb_.SelectedIndexChanged
+
+    End Sub
+
+
+
+    Private Sub btn_2_Click(sender As Object, e As EventArgs) Handles btn_2.Click
+
+    End Sub
+
+    Private Sub home_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
